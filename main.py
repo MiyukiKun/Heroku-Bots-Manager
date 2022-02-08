553a7e2fa84365a9e714a0bdbd33612997795c75
MiyukiKun <miyukibot1@gmail.com>
2022-02-08 17:02:23 +0530

    Start All

--- main.py
+++ main.py
@@ -11,16 +11,17 @@ async def _(event):
     if event.text == '/start' or event.text == '/start@Dio_Brando_Robot':
         await event.reply("You were expecting a welcome but,", file='https://c.tenor.com/WI1--JsW33EAAAAC/dio-jojo.gif')
         
+def start_bot(username, scale):
+    data = apidb.find({'_id':username})
+    heroku3.from_key(data['key'][1]).apps()[data['key'][0]].process_formation()['worker'].scale(scale)
+
 
 @bot.on(events.NewMessage(pattern=f"/off", chats=main_group_id))
 async def _(event):
     try:
         username = event.text.replace('/off', '')
         username = username.strip()
-        print(username)
-        data = apidb.find({'_id':username})
-        print(data)
-        heroku3.from_key(data['key'][1]).apps()[data['key'][0]].process_formation()['worker'].scale(0)
+        start_bot(username, 0)
         await event.reply(f"{username} is now dead.")
     except Exception as e:
         await event.reply(str(e))
@@ -30,9 +31,26 @@ async def _(event):
     try:
         username = event.text.replace('/on', '')
         username = username.strip()
-        data = apidb.find({'_id':username})
-        heroku3.from_key(data['key'][1]).apps()[data['key'][0]].process_formation()['worker'].scale(1)
+        start_bot(username, 1)
         await event.reply(f"{username} is now Alive.")
+    except Exception as e:
+        await event.reply(str(e))
+
+@bot.on(events.NewMessage(pattern=f"/allon", chats=main_group_id))
+async def _(event):
+    try:
+        for i in apidb.full():
+            start_bot(i['_id'], 1)
+            await event.reply(f"{i['_id']} is now Alive.")
+    except Exception as e:
+        await event.reply(str(e))
+
+@bot.on(events.NewMessage(pattern=f"/alloff", chats=main_group_id))
+async def _(event):
+    try:
+        for i in apidb.full():
+            start_bot(i['_id'], 0)
+            await event.reply(f"{i['_id']} is now Dead.")
     except Exception as e:
         await event.reply(str(e))
 
@@ -63,8 +81,6 @@ async def _(event):
         await event.reply(s)
     except Exception as e:
         await event.reply(e)
-  
-
 
 bot.start()
 
